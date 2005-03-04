0a5ef323305b1972d693da0e91a3af5ffcfc7bc8
Vincent Blavet <vblavet@php.net>
2005-03-04 22:32:42 +0000

    - Try to fix Bug #948 : freeze on pear install ...
    git-svn-id: https://svn.php.net/repository/pear/packages/Archive_Tar/trunk@181351 c90b9560-bf6c-de11-be94-00142212c4b1

--- Archive/Tar.php
+++ Archive/Tar.php
@@ -745,13 +745,10 @@ class Archive_Tar extends PEAR
     // }}}
 
     // {{{ _readBlock()
-    function _readBlock($p_len=null)
+    function _readBlock()
     {
       $v_block = null;
       if (is_resource($this->_file)) {
-          if ($p_len === null)
-              $p_len = 512;
-              
           if ($this->_compress_type == 'gz')
               $v_block = @gzread($this->_file, 512);
           else if ($this->_compress_type == 'bz2')
@@ -759,8 +756,8 @@ class Archive_Tar extends PEAR
           else if ($this->_compress_type == 'none')
               $v_block = @fread($this->_file, 512);
           else
-              $this->_error('Unknown or missing compression type ('.$this->_compress_type.')');
-
+              $this->_error('Unknown or missing compression type ('
+			                .$this->_compress_type.')');
       }
       return $v_block;
     }
@@ -773,8 +770,20 @@ class Archive_Tar extends PEAR
           if ($p_len === null)
               $p_len = 1;
 
-          if ($this->_compress_type == 'gz')
+          if ($this->_compress_type == 'gz') {
+              // ----- Add check of the gzseek() (trying to solve bug 948)
+              $v_old_pos = @gztell($this->_file);
               @gzseek($this->_file, @gztell($this->_file)+($p_len*512));
+              $v_new_pos = @gztell($this->_file);
+              if ($v_new_pos == 0) {
+                  $this->_error('Buggy gztell() function ..."
+				                ." (please update bug 948)');
+              }
+              if ($v_new_pos < $v_old_pos) {
+                  $this->_error('Buggy gzseek() function ..."
+				                ." (please update bug 948)');
+              }
+          }
           else if ($this->_compress_type == 'bz2') {
               // ----- Replace missing bztell() and bzseek()
               for ($i=0; $i<$p_len; $i++)
@@ -1341,7 +1350,7 @@ class Archive_Tar extends PEAR
 
     clearstatcache();
 
-    While (strlen($v_binary_data = $this->_readBlock()) != 0)
+    while (strlen($v_binary_data = $this->_readBlock()) != 0)
     {
       $v_extract_file = FALSE;
       $v_extraction_stopped = 0;
