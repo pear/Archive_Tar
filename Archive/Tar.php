4efaf6f96bbe9429ef916c8cd8ce8cabb4c94dfe
Michiel Rook <mrook@php.net>
2009-08-18 21:04:05 +0000

    Bug #11871 - wrong result of ::listContent() if filename begins or ends with space
    git-svn-id: https://svn.php.net/repository/pear/packages/Archive_Tar/trunk@287467 c90b9560-bf6c-de11-be94-00142212c4b1

--- Archive/Tar.php
+++ Archive/Tar.php
@@ -1303,7 +1303,7 @@ class Archive_Tar extends PEAR
         }
 
         // ----- Extract the properties
-        $v_header['filename'] = trim($v_data['filename']);
+        $v_header['filename'] = $v_data['filename'];
         if ($this->_maliciousFilename($v_header['filename'])) {
             $this->_error('Malicious .tar detected, file "' . $v_header['filename'] .
                 '" will not install in desired directory tree');
